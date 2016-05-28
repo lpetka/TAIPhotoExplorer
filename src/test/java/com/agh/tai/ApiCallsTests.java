598b66e00c35bc0f787116a398fc086f6fe7a39c
mzielu <kmarcinzielinski@gmail.com>
2016-05-28 17:07:02 +0200

    ApiCallsTests updated

--- src/test/java/com/agh/tai/ApiCallsTests.java
+++ src/test/java/com/agh/tai/ApiCallsTests.java
@@ -2,12 +2,28 @@ package com.agh.tai;
 
 import com.agh.tai.services.UserService;
 
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class ApiCallsTests {
 
     public static void main(String args[])
     {
-        UserService userService = new UserService();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Program created to test api calls used in the project. Please enter credentials (userName and accessToken)\t");
+
+        System.out.println("UserName:\t");
+        String userName = scanner.nextLine();
+
+        System.out.println("AccessToken:\t");
+        String accessToken = scanner.nextLine();
+
+        UserService userService = new UserService(userName, accessToken);
 
         userService.getImageByID(5324);
+        userService.uploadImageAnonymouslyByUrl("http://www.gojackal.com/wp-content/uploads/2015/09/Photoshop-Fail-17.jpg");
+        userService.getUserFavourites();
+        userService.getUserImages(0);
     }
 }
