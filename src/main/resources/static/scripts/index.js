53fea0ba81590142627031cbc6cd87a9235f3384
Łukasz Petka <petka@student.agh.edu.pl>
2016-06-29 15:06:06 +0200

    change localStorage to sessionStorage

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -6,15 +6,15 @@ $(document).ready(function () {
     }
     if(!!params["access_token"] && !!params["account_id"] && !!params["expires_in"] && !!params["account_username"]
         && !!params["token_type"] && !!params["refresh_token"]) {
-
-        if (!(!!localStorage.getItem("myParams"))) {
-            localStorage.setItem("myParams", JSON.stringify(params));
+        console.log("tu");
+        sessionStorage.setItem("myParams", JSON.stringify(params));
+        if (!(!!sessionStorage.getItem("myParams"))) {
             $.ajax({
                 type: "POST",
                 contentType: 'application/json',
                 dataType: 'json',
                 url: "/user/credentials",
-                data: localStorage.getItem("myParams"),
+                data: sessionStorage.getItem("myParams"),
                 success: function (result) {
                 }
             });
