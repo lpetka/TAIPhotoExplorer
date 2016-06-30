30f4e242653350d7fe547e48ea181ff82027763b
Łukasz Petka <petka@student.agh.edu.pl>
2016-06-30 04:10:14 +0200

    add displaying images from public gallery feature

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    parseUrl();
+    getImagesFromPage(0);
+
+    $('#loadMoreButton').click(function () {
+        getImagesFromPage(parseInt($('#pageNumber').val()) + 1);
+    });
+
+});
+
+function parseUrl() {
     var params = {}, queryString = location.hash.substring(1),
         regex = /([^&=]+)=([^&]*)/g, m;
     while (m = regex.exec(queryString)) {
@@ -20,5 +30,30 @@ $(document).ready(function () {
             });
         }
     }
+}
 
-});
+function getImagesFromPage(pageNumber) {
+    $.ajax({
+        type: "POST",
+        headers: {
+            "access_token" : JSON.parse(sessionStorage.getItem("myParams")).access_token,
+            "account_username": JSON.parse(sessionStorage.getItem("myParams")).account_username
+        },
+        contentType: 'application/json',
+        dataType: 'json',
+        url: "/public/gallery",
+        data: JSON.stringify(pageNumber),
+        success: function (result) {
+            appendImages(result.data, pageNumber)
+        }
+    });
+}
+
+function appendImages(data, pageNumber) {
+    if(data.length != 0)
+        $('#pageNumber').val(pageNumber);
+    $.each(data, function (index, item) {
+        var itemToAdd = '<div class="imageContainer"><input type="hidden" id="id" value="'+item.id+'"/><img class="img-thumbnail" src="'+item.link+'" /></div>';
+        $(itemToAdd).insertBefore('#buttonPanel');
+    })
+}
